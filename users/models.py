a29880014e5cd65f7f984d9c9f285b5aa192295f
Iris <s.eda@bk.ru>
2023-12-27 07:39:56 +0600

    ENDgit add .!

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from base.models import Vacancy
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -10,6 +12,7 @@ class Profile(models.Model):
         return f'{self.user.username} Profile'
     
 class InMessages(models.Model):
+    vacs = models.ForeignKey(Vacancy, on_delete=models.CASCADE, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     topic = models.CharField(max_length=30)
     text = models.TextField()
